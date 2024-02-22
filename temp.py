9b11d2c54f7afc58090a0918a82fe4d2928a6d9d
shashank-cognitica <shashank.babu@cognitica.ai>
2024-02-22 17:22:32 +0530

    inital commit

---
+++ temp.py
@@ -0,0 +1,9 @@
+print('working')
+
+# python generate_tfrecord.py -x /home/cognitica-i7-13thgen/NPS/xml_tfrec/PPEDEV02/train -l /home/cognitica-i7-13thgen/NPS/xml_tfrec/safety_label_map.pbtxt -o /home/cognitica-i7-13thgen/NPS/xml_tfrec/results/train.record
+
+# working
+# python generate_tfrecord.py -x /home/cognitica-i7-13thgen/NPS/xml_tfrec/PPEDEV02/train -l /home/cognitica-i7-13thgen/NPS/xml_tfrec/safety_label_map.pbtxt -o /home/cognitica-i7-13thgen/NPS/xml_tfrec/results/train.record
+
+
+# /home/cognitica-i7-13thgen/NPS/xml_tfrec
