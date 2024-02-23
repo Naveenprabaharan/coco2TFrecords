295ad7b61b9592c0cabadad262a15ddb505f7809
cognitica-naveen <naveenpraba.s@cognitica.ai>
2024-02-23 17:17:05 +0530

    req.txt chnaged

--- temp.py
+++ temp.py
@@ -1,9 +1,9 @@
 print('working')
 
-# python generate_tfrecord.py -x /home/cognitica-i7-13thgen/NPS/xml_tfrec/PPEDEV02/train -l /home/cognitica-i7-13thgen/NPS/xml_tfrec/safety_label_map.pbtxt -o /home/cognitica-i7-13thgen/NPS/xml_tfrec/results/train.record
+# python generate_tfrecord.py -x /home/cognitica_ai_user/NPS/Data/PPEDEV02.v1i.voc/test -l /home/cognitica_ai_user/NPS/Data/PPEDEV02.v1i.voc/safety_label_map.pbtxt -o /home/cognitica_ai_user/NPS/Github/xml2TFrecords/results/test.record
 
 # working
-# python generate_tfrecord.py -x /home/cognitica-i7-13thgen/NPS/xml_tfrec/PPEDEV02/train -l /home/cognitica-i7-13thgen/NPS/xml_tfrec/safety_label_map.pbtxt -o /home/cognitica-i7-13thgen/NPS/xml_tfrec/results/train.record
+# python generate_tfrecord.py -x /home/cognitica_ai_user/NPS/Data/PPEDEV02.v1i.voc/train -l /home/cognitica_ai_user/NPS/Data/PPEDEV02.v1i.voc/safety_label_map.pbtxt -o /home/cognitica_ai_user/NPS/Github/xml2TFrecords/results/train.record
 
 
 # /home/cognitica-i7-13thgen/NPS/xml_tfrec
